daa86ea2a99268c69a98fd117e93f6118f6f3707
Hemanth Gokavarapu <hemanth@soluble.ai>
2022-04-21 16:02:19 +0530

    check if high case is triggered
    Signed-off-by: Hemanth Gokavarapu <hemanth@soluble.ai>

--- terraform-gcp/kubernetes.tf
+++ terraform-gcp/kubernetes.tf
@@ -20,7 +20,6 @@ resource "google_container_cluster" "primary" {
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
     ]
   }
 }
