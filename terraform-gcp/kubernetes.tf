25f7c9707d8c47e84932f00f3b8dde7359adf74c
Hemanth Gokavarapu <hemanth@soluble.ai>
2022-04-21 16:05:24 +0530

    disable monitoring
    Signed-off-by: Hemanth Gokavarapu <hemanth@soluble.ai>

--- terraform-gcp/kubernetes.tf
+++ terraform-gcp/kubernetes.tf
@@ -15,11 +15,14 @@ resource "google_container_cluster" "primary" {
     password = var.password
   }
 
+  monitoring_service = "none"
+
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
     ]
   }
 }
