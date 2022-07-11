7dd3f50e47376b068e7364c04e98e05895cc8880
Hemanth Gokavarapu <hemanth@soluble.ai>
2022-07-11 16:37:19 +0530

    Add newline after config
    Signed-off-by: Hemanth Gokavarapu <hemanth@soluble.ai>

--- kubernetes.tf
+++ kubernetes.tf
@@ -23,8 +23,8 @@ resource "google_container_cluster" "primary" {
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
-      
     ]}
+
 }
 
 output "cluster_name" {
